cda7fc1d3afd9d0c027f5636dc80d69403c06d37
Olivia McKee <olivia.mckee97@gmail.com>
2022-06-04 17:39:59 -0500

    display icons fro weather conditions

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 var lat 
 var lon 
 var cityName
+var searchButtonEl
 
 var cityFormEl = document.getElementById("city-form");
 var cityInputEl = document.getElementById("city-input");
+var cityEl = document.getElementById("city");
+var recentSearchesEl = document.getElementById("recent-searches");
 
 //current date
 var dateEl = document.getElementById("date");
@@ -73,7 +76,24 @@ var formSubmitHandler = function(event) {
     if (cityName) {
         getCity(cityName);
         cityInputEl.value = "";
+        cityEl.textContent = cityName
+        recentSearchHandler();
     }
+}
+
+//save recent searches 
+var recentSearchHandler = function() {
+    searchButtonEl = document.createElement("button");
+    searchButtonEl.textContent = cityName;
+    searchButtonEl.setAttribute("class", "recent-search-button");
+    recentSearchesEl.appendChild(searchButtonEl);
+    searchButtonEl.addEventListener("click", recentSearchButton); 
+}
+
+var recentSearchButton = function() {
+    console.log(searchButtonEl.textContent);
+    cityName = this.textContent;
+    formSubmitHandler();
 }
 
 //get lat and lon coordinates for weather data api request 
@@ -83,11 +103,8 @@ var getCity = function() {
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-                console.log(data);
                 lat = data.coord.lat;
                 lon = data.coord.lon;
-                console.log(lat);
-                console.log(lon);
                 getWeatherData();
             })
         }
@@ -101,35 +118,34 @@ var getWeatherData = function() {
         .then(function(response) {
             if(response.ok) {
                 response.json().then(function(data) {
-                    // console.log(data);
                     // display current weather data 
-                    iconEl.textContent = data.current.weather[0].id;
+                    iconEl.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png'>";
                     tempEl.textContent = data.current.temp;
                     windEl.textContent = data.current.wind_speed;
                     humidityEl.textContent = data.current.humidity;
                     uvIndexEl.textContent = data.current.uvi;
                     // display forecast day 1 weather data 
-                    icon1El.textContent = data.daily[0].weather[0].id;
+                    icon1El.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + "@2x.png'>";
                     temp1El.textContent = data.daily[0].temp.day;
                     wind1El.textContent = data.daily[0].wind_speed;
                     humidity1El.textContent = data.daily[0].humidity;
                     // display forecast day 2 weatehr data
-                    icon2El.textContent = data.daily[1].weather[0].id;
+                    icon2El.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png'>";
                     temp2El.textContent = data.daily[1].temp.day;
                     wind2El.textContent = data.daily[1].wind_speed;
                     humidity2El.textContent = data.daily[1].humidity;
                     // display forecast day 3 weather data 
-                    icon3El.textContent = data.daily[2].weather[0].id;
+                    icon3El.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png'>";
                     temp3El.textContent = data.daily[2].temp.day;
                     wind3El.textContent = data.daily[2].wind_speed;
                     humidity3El.textContent = data.daily[2].humidity;
                     // display forecast day 4 weather data 
-                    icon4El.textContent = data.daily[3].weather[0].id;
+                    icon4El.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png'>";
                     temp4El.textContent = data.daily[3].temp.day;
                     wind4El.textContent = data.daily[3].wind_speed;
                     humidity4El.textContent = data.daily[3].humidity;
                     // dispaly forecast day 5 weather data 
-                    icon5El.textContent = data.daily[4].weather[0].id;
+                    icon5El.innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png'>";
                     temp5El.textContent = data.daily[4].temp.day;
                     wind5El.textContent = data.daily[4].wind_speed;
                     humidity5El.textContent = data.daily[4].humidity;
@@ -141,10 +157,10 @@ var getWeatherData = function() {
     
 }
 
-
 getDate();
-// getWeatherData();
+
 
 
 // event listener 
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
